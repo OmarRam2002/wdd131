ab91d93a5d1676795c13b5bdad80c0df149bd1ef
omarram2002 <omarramirezasencio@gmail.com>
2026-05-30 21:11:35 -0600

    made changes

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -93,7 +93,7 @@ function createTempleCard(filteredtemples) {
     filteredtemples.forEach(temple => {
 
         let card = document.createElement("section");
-        let name = document.createElement("h3");
+        let name = document.createElement("h2");
         let location = document.createElement("p");
         let dedication = document.createElement("p");
         let area = document.createElement("p");
